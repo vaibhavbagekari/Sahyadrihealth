0f506f827fd01cf922ba166cb4b052dc3c05f0c7
vaibhavbagekari <134953230+vaibhavbagekari@users.noreply.github.com>
2024-06-02 13:26:28 +0530

    major updates are here

--- core/utils.py
+++ core/utils.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
-# from twilio.rest import Client
+
 
 def send_email_to_client(drData,patientData,slotData):
     html_content = render_to_string("email_template1.html",{"drData":drData,"patientData":patientData,"slotData":slotData})
@@ -27,24 +27,3 @@ def drAccountOpeningEmail(drData):
     recipient_list = [drData["email"]]
     print("hii")
     send_mail(subject,html_content,from_email,recipient_list, html_message=html_content)
-
-# def send_sms(to_number, message_body):
-#     # Your Account SID and Auth Token from twilio.com/console
-#     account_sid = 'AC30f0cbf46e6c80e79bc5ef662e99f18b'
-#     auth_token = 'd114e8465c3220d77d8661016f3442a1'
-    
-#     # Initialize the Twilio client
-#     client = Client(account_sid, auth_token)
-    
-#     # The Twilio phone number you purchased
-#     from_number = '+17075498588'
-    
-#     # Send the SMS
-#     message = client.messages.create(
-#         body=message_body,
-#         from_=from_number,
-#         to=to_number
-#     )
-    
-#     # Print the message SID to confirm that the message was sent
-#     print(f'Message sent to {to_number}: {message.sid}')
